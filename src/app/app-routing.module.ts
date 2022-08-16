20fe66e8fda90fc0ed75c1e936f352e4daf1cd94
Viviane <vi.priscilasouza@gmail.com>
2022-08-15 21:09:31 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Erro404Component } from './erro404/erro404.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: '', component:LayoutComponent, children:[
+    {path:'home', component:HomeComponent},
+    {path:'usuario', component:UsuarioComponent},
+    {path: '**', component:Erro404Component},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { Erro404Component } from './erro404/erro404.component';
+import { LoginComponent } from './login/login.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { usuariosRoutingModule } from './usuarios/usuarios-routing.module.spec';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Erro404Component,
+    LoginComponent,
+    UsuariosComponent,
+    UsuarioComponent,
+    LayoutComponent,
+    HomeComponent,
+    
+
+
+
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TemplateModule,
+    RouterModule,
+
+    usuariosRoutingModule,
     AppRoutingModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
